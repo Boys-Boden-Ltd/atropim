f0e29c3ce6bedd03ee1fd818c3293e0444d72328
Max Atkins <max.atkins1@gmail.com>
2025-12-18 16:41:08 +0000

    Installed custom CLI installer module during application install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,20 +2,28 @@
 set -e
 
 if [ ! -f "vendor/autoload.php" ]; then
-    echo ">> FRESH INSTALL: Starting Setup..."
+    echo ">> Fresh Install: Starting Setup..."
 
     if [ ! -d ".git" ]; then
         echo ">> Cloning Skeleton Structure..."
         git clone https://gitlab.atrocore.com/atrocore/skeleton-pim-no-demo.git .
     fi
 
-    echo ">> Running Installation..."
+    echo ">> Running Composer Installation..."
 
     php composer.phar self-update
     export COMPOSER_PROCESS_TIMEOUT=2000
     php composer.phar update -n
 
-    echo ">> INSTALLATION COMPLETE"
+    echo ">> Composer Installation Complete"
+
+    echo ">> Running Application Installation..."
+
+    php composer.phar config repositories.boysandbodencli vcs https://github.com/Boys-Boden-Ltd/atrocore-cli-install
+    php composer.phar require atrocore/cli-install dev-main
+
+    echo ">> Application Installation Complete"
+
 else
     echo ">> AtroPIM is already installed. Starting services..."
 fi
